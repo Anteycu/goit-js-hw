f17e0a629d630c91c20ed297babe3c3c97f171e5
Nicholas Trunov <anteycu@gmail.com>
2020-06-19 16:53:26 +0300

    uncomment 6 task for index GHPages

--- module-01/index.js
+++ module-01/index.js
@@ -133,29 +133,29 @@
 // 3 - если не null(преобразовать в число) добавить в тотал
 // 4 - после цикла вывести результат общую сумму в алерт
 
-// let total = 0;
+let total = 0;
 
-// do {
-//     let userInput = prompt("Please, enter number!");
-//     if (userInput === null) {
-//         console.log("Canceled by user, take your result!");
-//         break;
-//     }
-//     userInput = Number(userInput);
+do {
+    let userInput = prompt("Please, enter number!");
+    if (userInput === null) {
+        console.log("Canceled by user, take your result!");
+        break;
+    }
+    userInput = Number(userInput);
 
-//     const notNumber = Number.isNaN(userInput);
+    const notNumber = Number.isNaN(userInput);
 
-//     if (notNumber) {
-//         alert("Dont save this iteration, please, enter a number!");
-//         continue;
-//     }
+    if (notNumber) {
+        alert("Dont save this iteration, please, enter a number!");
+        continue;
+    }
 
-//     total += userInput;
-//     console.log("Saved this iteration!");
+    total += userInput;
+    console.log("Saved this iteration!");
 
-// } while (true);
+} while (true);
 
-// alert(`Sum of numbers = ${total}`);
+alert(`Sum of numbers = ${total}`);
 
 // ========================================================================
 // Попытка без континью и с более сложным иф условием, но понял, что там буль, получилось только заменить на элсе.
