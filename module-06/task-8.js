07bc007bf4bcd3efb32c59b0e8a4e69f702b156f
Nicholas Trunov <anteycu@gmail.com>
2020-08-19 12:22:35 +0300

    checked before mentor observe

--- module-06/task-8.js
+++ module-06/task-8.js
@@ -1,37 +1,31 @@
-"use strict"
-import {
-    default as users
-} from "./users.js"
+"use strict";
+import { default as users } from "./users.js";
 
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
-// =============================tried 1st time==========================================
-const getUsersWithFriend = (users, friendName) => {
-    const NameIsFriend = users.filter(function (user) {
-        // console.log(user.friends);
-
-        if (user.friends.includes(friendName)) {
-            return {
-                // ...user.name --- Почему сюда не заглядывает исполнение, что бы я внутри не поставил, без разницы?
-            }
-        };
-    });
-    return NameIsFriend.map((user => user.name));
-};
-
-// =============================2рой вариант, сложнодоступный мне==============================================
-
+// ================================Variant 1==========================================
 // const getUsersWithFriend = (users, friendName) => {
-//     const result = users.reduce((acc, user) => {
-//         user.friends.forEach((friend) => {
-//             if (friend === friendName) {
-//                 acc.push(user.name)
-//             };
-//         });
-//         return acc
-//     }, []);
-//     return result
+//   const NameIsFriend = users.filter(function (user) {
+//     if (user.friends.includes(friendName)) {
+//       return {}; // почему сработал пустой литерал?
+//     }
+//   });
+//   return NameIsFriend.map((user) => user.name);
 // };
 
-console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+// ============================Variant 2 more difficult==============================================
+
+const getUsersWithFriend = (users, friendName) => {
+  const result = users.reduce((acc, user) => {
+    user.friends.forEach((friend) => {
+      if (friend === friendName) {
+        acc.push(user.name);
+      }
+    });
+    return acc;
+  }, []);
+  return result;
+};
+
+console.log(getUsersWithFriend(users, "Briana Decker")); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, "Goldie Gentry")); // [ 'Elma Head', 'Sheree Anthony' ]
--- module-06/task-9.js
+++ module-06/task-9.js
@@ -1,25 +1,12 @@
-"use strict"
-import {
-    default as users
-} from "./users.js"
+"use strict";
+import { default as users } from "./users.js";
 
 // Массив имен(поле name) людей, отсортированных в зависимости от количества их друзей(поле friends)
 
-// =================не знаю почему андэфайнд и как связать отсортированный массив кол-ва друзей с именами объектов=======
-
-const getNamesSortedByFriendsCount = users => {
-    const numOfFriends = users.map(function (user) {
-        user.friends.lenght;
-
-    });
-    numOfFriends.sort(function (a, b) {
-        a.numOfFriends - b.numOfFriends
-    });
-
+const getNamesSortedByFriendsCount = (users) => {
+  const fn = (a, b) => a.friends.length - b.friends.length;
+  return users.sort(fn).map((user) => user.name);
 };
-
-
-
 
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
