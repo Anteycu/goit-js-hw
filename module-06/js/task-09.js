4c1793db29320d51dce31ab9f670c4b661de199c
Nikolay Trunov <anteycu@gmail.com>
2024-01-13 19:02:28 +0200

    finished task-09

--- module-06/js/task-09.js
+++ module-06/js/task-09.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widgetRef = document.querySelector(".widget");
+const bodyRef = document.querySelector("body");
+const widgetElemArr = [...widgetRef.children];
+const buttonRef = widgetElemArr.find(
+  (elem) => elem.className === "change-color"
+);
+const pRef = widgetElemArr.find((elem) => elem.children);
+const spanRef = [...pRef.children].find((elem) => elem.className === "color");
+
+function changeColorHandler() {
+  const generatedColor = getRandomHexColor();
+  bodyRef.style.backgroundColor = generatedColor;
+  spanRef.textContent = generatedColor;
+}
+
+buttonRef.addEventListener("click", changeColorHandler);
