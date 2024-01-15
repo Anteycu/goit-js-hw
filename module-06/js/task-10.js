cb9137ac7d3328f54dbc776d87aa892dacc3c40e
Nikolay Trunov <anteycu@gmail.com>
2024-01-15 16:00:00 +0200

    finished all tasks from module-06

--- module-06/js/task-10.js
+++ module-06/js/task-10.js
@@ -3,3 +3,52 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createInitialArr(num, initialSize = 30) {
+  const initialArr = [];
+  let elemSize = initialSize;
+
+  for (let i = 0; i < Number(num); i += 1) {
+    const step = 10;
+    initialArr.push(elemSize);
+    elemSize += step;
+  }
+  return initialArr;
+}
+
+const createBoxes = (num) => {
+  const boxesMarkup = createInitialArr(num).reduce((acc, elem) => {
+    const box = `<div style="background: ${getRandomHexColor()}; width: ${elem}px; height: ${elem}px"></div>`;
+    acc += box;
+    return acc;
+  }, "");
+  return boxesMarkup;
+};
+
+const destroyBoxes = () => {
+  collectionRef.innerHTML = "";
+};
+
+const collectionRef = document.querySelector("#boxes");
+const controlsRef = document.querySelector("#controls");
+const controlsElemArr = [...controlsRef.children];
+
+const createBtnRef = controlsElemArr.find((elem) =>
+  elem.dataset.hasOwnProperty("create")
+);
+const destroyBtnRef = controlsElemArr.find((elem) =>
+  elem.dataset.hasOwnProperty("destroy")
+);
+
+createBtnRef.addEventListener("click", createHandler);
+destroyBtnRef.addEventListener("click", destroyBoxes);
+
+function createHandler(evt) {
+  const {
+    target: { previousElementSibling },
+  } = evt;
+  collectionRef.insertAdjacentHTML(
+    "beforeend",
+    createBoxes(previousElementSibling.value)
+  );
+}
