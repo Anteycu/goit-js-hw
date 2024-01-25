fd25a5b0f14b96568856baa95fb1f2f7ea537f95
Nikolay Trunov <anteycu@gmail.com>
2024-01-25 17:14:54 +0200

    done second task and fix double overlay bug

--- module-07/js/02-lightbox.js
+++ module-07/js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+galleryRef.addEventListener("click", onClick);
 
-console.log(galleryItems);
+function onClick(evt) {
+  evt.preventDefault();
+  const { classList } = evt.target;
+  if (!classList.contains("gallery__image")) {
+    return;
+  }
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+  lightbox.on("closed.simplelightbox", function () {
+    lightbox.refresh();
+  });
+}
+
+const createMarkup = (arr) => {
+  return arr.map((item) => {
+    const { preview, original, description } = item;
+    return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+</li>`;
+  });
+};
+galleryRef.insertAdjacentHTML("beforeend", createMarkup(galleryItems).join(""));
